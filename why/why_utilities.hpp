bb2bd5406a54a113d209f0a5a59f74f633c023d4
vdell <ville.lindell@gmail.com>
2014-07-19 09:10:58 +0300

    Removed unused utility function

--- why/why_utilities.hpp
+++ why/why_utilities.hpp
@@ -3,24 +3,4 @@
 namespace why
 {
 	std::string get_os_info();
-
-	// http://stackoverflow.com/questions/800955/remove-if-equivalent-for-stdmap
-	namespace generic 
-	{
-		template <typename ContainerT, typename PredicateT>
-		void erase_if(ContainerT& items, const PredicateT& predicate) 
-		{
-			for (auto it = items.begin(); it != items.end();) 
-			{
-				if (predicate(*it))
-				{
-					it = items.erase(it);
-				}
-				else
-				{
-					++it;
-				}
-			}
-		};
-	}
 }
