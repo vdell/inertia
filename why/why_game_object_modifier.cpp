d48885c661b864db4548c246ae13772a9c1a4583
vdell <ville.lindell@gmail.com>
2014-08-04 07:03:03 +0300

    Add try/catch block and use floating point format

--- why/why_game_object_modifier.cpp
+++ why/why_game_object_modifier.cpp
@@ -46,58 +46,72 @@ void why::WidthModifier::apply()
 {
 	using namespace clan;
 
-	CollidableObject &o = dynamic_cast <CollidableObject &>(m_obj);
-	o.set_scale(m_scale_x, 1.0f);
-
-	clan::PolygonShape *ps = dynamic_cast<clan::PolygonShape *>(&m_obj);
-	if (ps)
+	try
 	{
-		ps->set_as_box((m_obj.get_width() * m_scale_x)/2, m_obj.get_height()/2);
+		CollidableObject &o = dynamic_cast <CollidableObject &>(m_obj);
+		o.set_scale(m_scale_x, 1.0f);
+
+		clan::PolygonShape *ps = dynamic_cast<clan::PolygonShape *>(&m_obj);
+		if (ps)
+		{
+			ps->set_as_box((m_obj.get_width() * m_scale_x) / 2.0f, m_obj.get_height() / 2.0f);
+		}
+
+		FixtureDescription fd = o.fixture_description(m_pc);
+		fd.set_shape(dynamic_cast<clan::Shape &>(o));
+		BodyDescription bd = o.body_description(m_pc);
+
+		Body b(m_pc, bd);
+		b.set_data(&o);
+		Fixture f(m_pc, b, fd);
+
+		o.body().kill();
+		o.fixture().kill();
+
+		o.body() = b;
+		o.fixture() = f;
 	}
-
-	FixtureDescription fd = o.fixture_description(m_pc);
-	fd.set_shape(dynamic_cast<clan::Shape &>(o));
-	BodyDescription bd = o.body_description(m_pc);
-
-	Body b(m_pc, bd);
-	b.set_data(&o);
-	Fixture f(m_pc, b, fd);
-
-	o.body().kill();
-	o.fixture().kill();
-
-	o.body() = b;
-	o.fixture() = f;
+	catch (std::exception &e)
+	{
+		WHY_LOG() << e.what();
+	}
 }
 
 void why::WidthModifier::reset()
 {
 	using namespace clan;
 
-	if (m_is_reset) return;
-
-	CollidableObject &o = dynamic_cast <CollidableObject &>(m_obj);
-	o.set_scale(1.0f, 1.0f);
-
-	clan::PolygonShape *ps = dynamic_cast<clan::PolygonShape *>(&m_obj);
-	if (ps)
+	try
 	{
-		ps->set_as_box(m_obj.get_width()/2, m_obj.get_height()/2);
+		if (m_is_reset) return;
+
+		CollidableObject &o = dynamic_cast <CollidableObject &>(m_obj);
+		o.set_scale(1.0f, 1.0f);
+
+		clan::PolygonShape *ps = dynamic_cast<clan::PolygonShape *>(&m_obj);
+		if (ps)
+		{
+			ps->set_as_box(m_obj.get_width() / 2.0f, m_obj.get_height() / 2.0f);
+		}
+
+		FixtureDescription fd = o.fixture_description(m_pc);
+		fd.set_shape(dynamic_cast<clan::Shape &>(o));
+		BodyDescription bd = o.body_description(m_pc);
+
+		Body b(m_pc, bd);
+		b.set_data(&o);
+		Fixture f(m_pc, b, fd);
+
+		o.body().kill();
+		o.fixture().kill();
+
+		o.body() = b;
+		o.fixture() = f;
+
+		m_is_reset = true;
 	}
-
-	FixtureDescription fd = o.fixture_description(m_pc);
-	fd.set_shape(dynamic_cast<clan::Shape &>(o));
-	BodyDescription bd = o.body_description(m_pc);
-
-	Body b(m_pc, bd);
-	b.set_data(&o);
-	Fixture f(m_pc, b, fd);
-
-	o.body().kill();
-	o.fixture().kill();
-
-	o.body() = b;
-	o.fixture() = f;
-
-	m_is_reset = true;
+	catch (std::exception &e)
+	{
+		WHY_LOG() << e.what();
+	}
 }
